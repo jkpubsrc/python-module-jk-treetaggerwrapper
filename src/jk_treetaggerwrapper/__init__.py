7542692197f74146c53c73776bcdd6344bdd1676
jkpubsrc <pubsrc@binary-overflow.de>
2018-12-28 09:56:59 +0100

    Added module version string.

--- src/jk_treetaggerwrapper/__init__.py
+++ src/jk_treetaggerwrapper/__init__.py
@@ -3,3 +3,8 @@
 from .PoolOfThreadedTreeTaggers2 import PoolOfThreadedTreeTaggers
 
 
+__version__ = "0.2018.12.28"
+
+
+
+
--- src/setup.py
+++ src/setup.py
@@ -7,13 +7,13 @@ def readme():
 
 
 setup(name = "jk_treetaggerwrapper",
-	version = "0.2018.12.19.2",
+	version = "0.2018.12.28",
 	description = "This python module provides a wrapper around treetagger.",
 	author = "Jürgen Knauth",
 	author_email = "pubsrc@binary-overflow.de",
 	license = "Apache 2.0",
 	url = "https://github.com/jkpubsrc/python-module-jk-treetaggerwrapper",
-	download_url = "https://github.com/jkpubsrc/python-module-jk-treetaggerwrapper/tarball/0.2018.12.19.2",
+	download_url = "https://github.com/jkpubsrc/python-module-jk-treetaggerwrapper/tarball/0.2018.12.28",
 	keywords = [
 		"treetagger",
 		"pos-tagging"
